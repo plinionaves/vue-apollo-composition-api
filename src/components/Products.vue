b3bd2040c590e0fac09e7f50c0e0e8aa7a61695f
Plinio Naves <pliniopjn@hotmail.com>
2020-01-09 16:04:50 -0300

    added loading

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,9 +1,12 @@
 <template>
-  <ul>
-    <li v-for="product in products" :key="product.id">
-      {{ product.name }} | R$ {{ product.price }}
-    </li>
-  </ul>
+  <div>
+    <h2 v-if="loading">Loading...</h2>
+    <ul v-else>
+      <li v-for="product in products" :key="product.id">
+        {{ product.name }} | R$ {{ product.price }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,10 +19,13 @@ import productsQuery from '@/graphql/products.query.gql';
 export default createComponent({
   name: 'Products',
   setup() {
-    const { result } = useQuery<{ products: Product[] }>(productsQuery);
+    const { loading, result } = useQuery<{ products: Product[] }>(
+      productsQuery,
+    );
     const products = useResult(result, null, data => data.products);
 
     return {
+      loading,
       products,
     };
   },
